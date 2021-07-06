efdca9960e266004f9a8722bbe87441fdf826e0e
Alex Phun <alphun@ucsc.edu>
2021-07-05 19:23:34 -0700

    added rest of audio

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,11 +8,11 @@ class Menu extends Phaser.Scene {
     }
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        /*this.load.audio('sfx_select', './assets/blip_select12.wav');
 
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
 
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');*/
     }
     create() {
         let menuConfig = {
@@ -27,17 +27,17 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
             
         }
-        this.add.text(game.config.width/2, game.config.height/2 - (borderUISize + borderPadding), 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - (borderUISize + borderPadding), 'Pixel Runner', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use Spacebar to Jump', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = "#00FF00";
         menuConfig.color = "#000";
-        this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding), 'Press ← of Novice or → Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding), 'Press Spacebar to Start', menuConfig).setOrigin(0.5);
 
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
     /*update() {
-        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
             game.settings = {
                 spaceshipSpeed: 3,
                 gameTimer: 60000
@@ -45,15 +45,6 @@ class Menu extends Phaser.Scene {
             this.sound.play('sfx_select');
             this.scene.start('playScene');
         }
-        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-            game.settings = {
-                spaceshipSpeed: 4,
-                gameTimer: 45000
-            }
-            this.sound.play('sfx_select');
-            this.scene.start('playScene');
-        }
-
     }*/
 }
 
